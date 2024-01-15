e22e69eedb7cee82ac12514a27e5b088cb20d549
Thomas McMahon <glassoctopus@yahoo.com>
2024-01-15 10:46:20 -0600

    finish up author portion, copYpasta in place, not fully implemented.

---
+++ components/AuthorCard.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import Link from 'next/link';
+import { deleteSingleAuthor } from '../api/authorData';
+
+function authorCard({ authorObj, onUpdate }) {
+  // FOR DELETE, WE NEED TO REMOVE THE author AND HAVE THE VIEW RERENDER,
+  // SO WE PASS THE FUNCTION FROM THE PARENT THAT GETS THE authorS
+  const deleteThisauthor = () => {
+    if (window.confirm(`Delete ${authorObj.title}?`)) {
+      deleteSingleAuthor(authorObj.firebaseKey).then(() => onUpdate());
+    }
+  };
+
+  return (
+    <Card style={{ width: '18rem', margin: '10px' }}>
+      <Card.Img variant="top" src={authorObj.image} alt={authorObj.title} style={{ height: '400px' }} />
+      <Card.Body>
+        <Card.Title>{authorObj.title}</Card.Title>
+        <p className="card-text bold">{authorObj.sale && <span>SALE<br /></span> } ${authorObj.price}</p>
+        {/* DYNAMIC LINK TO VIEW THE author DETAILS  */}
+        <Link href={`/author/${authorObj.firebaseKey}`} passHref>
+          <Button variant="primary" className="m-2">VIEW</Button>
+        </Link>
+        {/* DYNAMIC LINK TO EDIT THE author DETAILS  */}
+        <Link href={`/author/edit/${authorObj.firebaseKey}`} passHref>
+          <Button variant="info">EDIT</Button>
+        </Link>
+        <Button variant="danger" onClick={deleteThisauthor} className="m-2">
+          DELETE
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+authorCard.propTypes = {
+  authorObj: PropTypes.shape({
+    image: PropTypes.string,
+    title: PropTypes.string,
+    sale: PropTypes.bool,
+    price: PropTypes.string,
+    firebaseKey: PropTypes.string,
+  }).isRequired,
+  onUpdate: PropTypes.func.isRequired,
+};
+
+export default authorCard;
