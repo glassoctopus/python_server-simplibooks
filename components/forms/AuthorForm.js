00856b7ec0114a7f3c77e5c55612f112a29c1c1f
Thomas McMahon <glassoctopus@yahoo.com>
2024-01-16 21:18:27 -0600

    forms up, delete books works, need to ensure data is going to the database

--- components/forms/AuthorForm.js
+++ components/forms/AuthorForm.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useRouter } from 'next/router';
 import PropTypes from 'prop-types';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
